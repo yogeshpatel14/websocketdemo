2bd0b983ffcd59aaf6c392f0bd4f092548f9d13c
Yogesh Patel <35160347+yogeshpatel14@users.noreply.github.com>
2020-08-05 15:51:17 +0530

    init code

---
+++ precache-manifest.d2edaa814ac215c5fe84eac4f67a9fe0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14e27ae3bf84397f5775a42f4d28ccd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a535dd3163537cda58e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1af86e9939b1be8c1754",
+    "url": "/static/js/2.7e066f2e.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.7e066f2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a535dd3163537cda58e",
+    "url": "/static/js/main.a87cd19d.chunk.js"
+  },
+  {
+    "revision": "33c631fc453854427a68",
+    "url": "/static/js/runtime-main.ecd3b2a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

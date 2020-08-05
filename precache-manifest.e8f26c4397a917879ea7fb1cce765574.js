c3ab781933be5e70c5b1143193bee738cb5ebc9b
yogesh-at-512770843289 <yogesh@mycom.world>
2020-08-05 16:28:49 +0530

    Updates

---
+++ precache-manifest.e8f26c4397a917879ea7fb1cce765574.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20037c2edb3fb3f819e830ac53936a7d",
+    "url": "/websocketdemo/index.html"
+  },
+  {
+    "revision": "c7d5d0e94b84a18aae4d",
+    "url": "/websocketdemo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a29d42cb449e6380abc5",
+    "url": "/websocketdemo/static/js/2.ce656769.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/websocketdemo/static/js/2.ce656769.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7d5d0e94b84a18aae4d",
+    "url": "/websocketdemo/static/js/main.408a9ca4.chunk.js"
+  },
+  {
+    "revision": "9210c6cc280044a189a4",
+    "url": "/websocketdemo/static/js/runtime-main.38abd99b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/websocketdemo/static/media/logo.5d5d9eef.svg"
+  }
+]);
